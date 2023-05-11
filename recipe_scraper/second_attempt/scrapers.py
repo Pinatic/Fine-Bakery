0df7bd460c3e7045fb068ba414b5d79b1705e1e3
JMenzingaST <jacobmenzinga@gmail.com>
2023-05-11 16:58:31 +0200

    I Think I might've solved it. Need to async it up though.

--- recipe_scraper/second_attempt/scrapers.py
+++ recipe_scraper/second_attempt/scrapers.py
@@ -6,6 +6,7 @@ import os
 import pandas as pd
 import numpy as np
 from recipe_scrapers import scrape_me
+import csv
 
 
 class LekkerEnSimpelScraper:
@@ -15,9 +16,9 @@ class LekkerEnSimpelScraper:
 
     def __init__(self) -> None:
         self.recipes = None
-        self.base_recep_url = 'https://www.lekkerensimpel.com/'
+        self.base_recipe_url = 'https://www.lekkerensimpel.com/'
 
-    def get_recipes(self):
+    def get_recipes(self, destination, start_at=1):
         """
         Goes through all the pages of all the food categories and stores the
         recipes in a list. Any duplicates are removed from this list and then
@@ -29,28 +30,64 @@ class LekkerEnSimpelScraper:
 
         recipes = []
 
-        for tag in tags[:2]:
-            print(f'working on {tag}')
-            page_num = 1
-            page_url = f'https://www.lekkerensimpel.com/{tag}/page/{page_num}/'
+        if not os.path.exists(destination):
+            os.makedirs(destination)
 
-            while page_num < 5:
-                print(f'page number: {page_num}')
-                html = requests.get(page_url)
+        with open(destination+'smulweb_recep_nrs.txt', 'w') as recep_file:
+            with open(destination+'smulweb_page_nr_reached.txt', 'w') as page_storer:
 
-                if 'class="cell large-6 medium-6 small-12"' in html.text:
+                for tag in tags[:2]:
+                    print(f'working on {tag}')
+                    page_num = start_at
+                    page_url = f'https://www.lekkerensimpel.com/{tag}/page/{page_num}/'
 
-                    soup = BeautifulSoup(html.text, 'html.parser')
+                    while page_num < 5:
+                        print(f'page number: {page_num}')
+                        html = requests.get(page_url)
 
-                    [recipes.append(a['href'].split('/')[-2]) for a in
-                     (soup.find_all('a', attrs={"class": "post-item__anchor"}))]
+                        if 'class="cell large-6 medium-6 small-12"' in html.text:
 
+                            soup = BeautifulSoup(html.text, 'html.parser')
+
+                            [recipes.append(a['href'].split('/')[-2]) for a in
+                             (soup.find_all('a', attrs={"class": "post-item__anchor"}))]
+
+                            page_num += 1
+
+                        else:
+                            break
+
+                self.recipes = list(set(recipes))
+
+    def get_numbers(self, destination, start_at=1):
+        """
+        Retrives all the recipe numbers from a range of lekker simpel
+        recipe pages
+        """
+
+        pattern = '/(\d{7})/'
+        page_num = start_at
+        if not os.path.exists(destination):
+            os.makedirs(destination)
+
+        with open(destination+'ls_recep_nrs.txt', 'w') as recep_file:
+            with open(destination+'ls_page_nr_reached.txt', 'w') as page_storer:
+
+                while page_num < 3:  # True for production
+                    print(f'Page {page_num}')
+
+                    # getting HTML
+                    r = requests.get(f'{self.base_search_url}{page_num}')
+
+                    # checking if there's still recipes on the page
+                    if 'Geen zoekresultaten gevonden.' in r.text:
+                        break
+
+                    else:
+                        numbers = re.findall(pattern=pattern, string=r.text)
+                        recep_file.write(str(numbers).strip('"[]'))
+                        page_storer.write(str(page_num)+'\n')
                     page_num += 1
-
-                else:
-                    break
-
-        self.recipes = list(set(recipes))
 
 
 class AlbertScraper:
@@ -64,31 +101,62 @@ class AlbertScraper:
         self.base_recipe_url = 'https://www.ah.nl/allerhande/recept/R-R'
         self.recipe_nums = None
 
-    def get_numbers(self):
+    # def get_numbers(self):
+    #     """
+    #     Retrives all the recipe numbers from a range of AH recipe pages
+    #     """
+
+        # pattern = 'R-R(\d{6,7})'
+        # numbers = []
+        # page_num = 0
+
+        # # while True:
+        # while True:  # For testing, to replace with while true
+        #     print(f'Page {page_num}')
+
+        #     # getting HTML
+        #     r = requests.get(f'{self.base_search_url}{page_num}')
+
+        #     # checking if there's still recipes on the page
+        #     if 'R-R' not in r.text:
+        #         break
+
+        #     else:
+        #         numbers += set(re.findall(pattern=pattern, string=r.text))
+        #         page_num += 1
+
+        # self.recipe_nums = numbers
+
+    def get_numbers(self, destination, start_at=0):
         """
-        Retrives all the recipe numbers from a range of AH recipe pages
+        Retrives all the recipe numbers from a range of lekker simpel
+        recipe pages
         """
 
         pattern = 'R-R(\d{6,7})'
-        numbers = []
-        page_num = 0
+        page_num = start_at
 
-        # while True:
-        while True:  # For testing, to replace with while true
-            print(f'Page {page_num}')
+        if not os.path.exists(destination):
+            os.makedirs(destination)
 
-            # getting HTML
-            r = requests.get(f'{self.base_search_url}{page_num}')
+        with open(destination+'ah_recep_nrs.txt', 'w') as recep_file:
+            with open(destination+'ah_page_nr_reached.txt', 'w') as page_storer:
 
-            # checking if there's still recipes on the page
-            if 'R-R' not in r.text:
-                break
+                while page_num < 3:  # True for production
+                    print(f'Page {page_num}')
 
-            else:
-                numbers += set(re.findall(pattern=pattern, string=r.text))
-                page_num += 1
+                    # getting HTML
+                    r = requests.get(f'{self.base_search_url}{page_num}')
 
-        self.recipe_nums = numbers
+                    # checking if there's still recipes on the page
+                    if 'Geen zoekresultaten gevonden.' in r.text:
+                        break
+
+                    else:
+                        numbers = re.findall(pattern=pattern, string=r.text)
+                        recep_file.write(str(numbers).strip('"[]'))
+                        page_storer.write(str(page_num)+'\n')
+                    page_num += 1
 
 
 class SmulwebScraper:
@@ -113,23 +181,23 @@ class SmulwebScraper:
         if not os.path.exists(destination):
             os.makedirs(destination)
 
-        with open(destination+'smulweb_recep_nrs.json', 'a') as recep_file:
+        with open(destination+'smulweb_recep_nrs.txt', 'w') as recep_file:
+            with open(destination+'smulweb_page_nr_reached.txt', 'w') as page_storer:
 
-            while page_num < 5:  # True for production
-                print(f'Page {page_num}')
+                while page_num < 3:  # True for production
+                    print(f'Page {page_num}')
 
-                # getting HTML
-                r = requests.get(f'{self.base_search_url}{page_num}')
+                    # getting HTML
+                    r = requests.get(f'{self.base_search_url}{page_num}')
 
-                # checking if there's still recipes on the page
-                if 'Geen zoekresultaten gevonden.' in r.text:
-                    break
+                    # checking if there's still recipes on the page
+                    if 'Geen zoekresultaten gevonden.' in r.text:
+                        break
 
-                else:
-                    numbers = re.findall(pattern=pattern, string=r.text)
-                    json.dump({page_num: numbers}, recep_file)
-                    recep_file.write('\n')
-
+                    else:
+                        numbers = re.findall(pattern=pattern, string=r.text)
+                        recep_file.write(str(numbers).strip('"[]'))
+                        page_storer.write(str(page_num)+'\n')
                     page_num += 1
 
 
@@ -141,36 +209,28 @@ class scrape_recipes:
     attributes:
     folder (str): The location where the recipes should be stored
     base_url (str): The base url that refers to the website you want to scrape
-    recipe_num_csv (csv file): A csv file containing the page,list of recipes of a
+    recipe_num_txt (csv file): A csv file containing the page,list of recipes of a
     scraped site
     """
 
-    def __init__(self, folder, base_url, recipe_num_csv) -> None:
+    def __init__(self, folder, base_url, recipe_num_txt) -> None:
         self.folder = folder
         self.base_url = base_url
-        self.recipe_num_csv = recipe_num_csv
+        self.recipe_num_txt = recipe_num_txt
         self.recipe_list = None
 
-    def create_list(self):  # THIS DONT WORK BECAUSE THE CSV IS TRASH. JSON MAYBE?!
-        # Reading in the csv
-        df = pd.read_csv(self.recipe_num_csv, header=None, delimiter='\t')
-
-        # Turning the recipe identifiers into a nested list
-        list_of_lists = df[1].to_list()
-        # Flattening the nested list
-
-        flat_list = [item for sublist in list_of_lists for item in sublist]
-        # from https://stackoverflow.com/questions/952914/how-do-i-make-a-flat-list-out-of-a-list-of-lists
-
-        self.recipe_list = list(set(flat_list))
+    def create_list(self):
+        with open(self.recipe_num_txt, 'r') as recep_file:
+            self.recipe_list = recep_file.readline().replace("'", "").split(',')
 
     def fetch_receps(self):
         n = 0
         "gets the receps in a recep list"
-        for recep in self.recipe_list[10]:
+        for recep in self.recipe_list[:10]:
             filename = '_'.join(recep.lower().split())+'.json'
 
             if os.path.isfile(self.folder+"/"+filename) is False:
+                print(f'{self.base_url}{recep}')
                 scraper = scrape_me(f'{self.base_url}{recep}')
 
                 if not os.path.exists(self.folder):
